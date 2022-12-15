f8233d5b9a360f54709464bbd2bfbd7fcedb79d7
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-12-15 22:40:19 +0100

    end second footer con non pochi problemi di identazione

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('titles','DC COMICS HOME')</title>
+    <title>@yield('titles', 'DC COMICS HOME')</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -16,12 +16,14 @@
 </head>
 
 <body>
-@include('partials.header')
-@include('partials.jumbo')
-<main>
-    @yield('content')
-</main>
-@include('partials.footer')
+    @include('partials.header')
+    @include('partials.jumbo')
+    <main>
+        @yield('content')
+    </main>
+    @include('partials.footer')
+
+
 </body>
 
 </html>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,36 +1,49 @@
 <?php
 $lists = config('menufooter');
 ?>
-<div>
-    <div id="primofoo" class="d-flex ">
-        <div class="container d-flex ">
+<div id="primofoo">
+    <div class="container">
+        <div class="liste d-flex">
+            @foreach ($lists as $list)
+                <div class="d-flex align-content-center ">
+                    <div class="d-flex  align-content-center">
 
-            <div class="liste ">
-                <div class="d-flex">
-
-                    @foreach ($lists as $list)
-                        <div class="d-flex align-items-center ">
-                            <div class="d-flex  align-items-center">
-
-                                <h4 class="text-white aling-items-start">{{ $list['title'] }}</h4>
-                                <ul class=" list-unstyled">
-                                    @foreach ($list['links'] as $lis)
-                                        <li class="me-1  "><a class="text-secondary"
-                                                href="{{ $lis['url'] }} ">{{ $lis['text'] }} </a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                    @endforeach
+                        <h4 class="text-white aling-items-start">{{ $list['title'] }}</h4>
+                        <ul class=" list-unstyled">
+                            @foreach ($list['links'] as $lis)
+                                <li class="me-1  "><a class="text-secondary"
+                                        href="{{ $lis['url'] }} ">{{ $lis['text'] }} </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
+            @endforeach
+
+            <div class="img">
+                <img src={{ Vite::asset('resources/images/dc-logo-bg.png') }} alt="">
             </div>
         </div>
     </div>
 </div>
-<div class="img">
-    <img src={{ Vite::asset('resources/images/dc-logo-bg.png') }} alt="">
-</div>
-</div>
-</div>
-<div id="secondofoo">
+<div id="secondofoo" class="bg-dark text-white p-5">
+    <div class="container d-flex justify-content-between ">
+        <div>
+            <button class="sign"> Sign-up now!</button>
+        </div>
+        <div class="social">
+
+            <span class="me-3 text-uppercase text-primary fw-bold">Follow us</span>
+            <img src={{ Vite::asset('resources/images/footer-facebook.png') }} alt="fb">
+            <img src={{ Vite::asset('resources/images/footer-twitter.png') }} alt="twitter">
+            <img src={{ Vite::asset('resources/images/footer-youtube.png') }} alt="yt">
+            <img src={{ Vite::asset('resources/images/footer-pinterest.png') }} alt="pinterst">
+            <img src={{ Vite::asset('resources/images/footer-periscope.png') }} alt="periscope">
+
+
+
+        </div>
+    </div>
+
 </div>
 </div>
